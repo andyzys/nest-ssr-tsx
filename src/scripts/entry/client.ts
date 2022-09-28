23e038eb6af38a9b9ca9c3cf885fa8418fdda5fa
zouyongsheng <zouyongsheng@kuaishou.com>
2022-09-28 17:08:21 +0800

    feat: 切换为rxui渲染

--- src/scripts/entry/client.ts
+++ src/scripts/entry/client.ts
@@ -3,6 +3,24 @@ import { COMPONENT_ENTRY_PATH, INJECT_DATA_SCRIPT_ID } from '../../common/consta
 export function getClientEntry() {
   return `
     import React from 'react'
+    import {render} from '@mybricks/rxui'
+    import Page from "${COMPONENT_ENTRY_PATH}";
+    
+    const parent = document.createElement('div')
+    parent.style.height = '100%'
+    document.body.appendChild(parent)
+    
+    const script = document.getElementById('${INJECT_DATA_SCRIPT_ID}')
+    
+    render(<Page {...JSON.parse(decodeURIComponent(script.dataset.obj))}/>, parent, () => {
+        script.replaceWith(...[...script.childNodes])
+    })  
+  `
+}
+
+export function getReactDomClientEntry() {
+  return `
+    import React from 'react'
     import ReactDOM from 'react-dom'
     import Page from "${COMPONENT_ENTRY_PATH}";
     
