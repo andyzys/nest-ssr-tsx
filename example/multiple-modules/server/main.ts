9c0bd888eb129a9d8b552dcd2e4b639847bd432a
zouyongsheng <zouyongsheng@kuaishou.com>
2022-10-09 16:09:36 +0800

    feat:

--- example/multiple-modules/server/main.ts
+++ example/multiple-modules/server/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+// @ts-ignore
+import compression from 'compression';
 
 const DEFAULT_PORT = 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   console.log('启动端口', process.env.PORT || DEFAULT_PORT)
+  app.use(compression());
   await app.listen(process.env.PORT || DEFAULT_PORT)
 }
 
