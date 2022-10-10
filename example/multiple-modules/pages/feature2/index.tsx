7a2eac776e0920a22d647bc7a5dd130539da1ca7
zouyongsheng <zouyongsheng@kuaishou.com>
2022-10-10 20:25:51 +0800

    feat: 增加scss依赖

--- example/multiple-modules/pages/feature2/index.tsx
+++ example/multiple-modules/pages/feature2/index.tsx
@@ -2,6 +2,8 @@ import React, {useState, useLayoutEffect, useEffect, useRef} from 'react'
 // @ts-ignore
 import css from './index.less'
 import Hello from './hello/test'
+// @ts-ignore
+import scss from './index.scss'
 
 export default function(props: any) {
   const [count, setCount] = useState(3)
@@ -12,8 +14,9 @@ export default function(props: any) {
   }, [])
   return (
     <div>
+      <h2 className={scss.big}>scss大号字体</h2>
       <h1 className={css.red}>{props.name}</h1>
-      <p className={css.green}>这是模块2With prettified output</p>
+      <p className={css.green}>这是模块2With prettified 22222</p>
       {count}
       <Hello />
       <Hello />
